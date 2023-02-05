a52ed982e035614939e47e1a49485c7f3ea2809e
Candice Smith MacKenzie <Mahony8@aol.com>
2023-02-05 17:43:04 -0500

    added castle seeds

---
+++ backend/db/seeds.rb
@@ -0,0 +1,49 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+#     t.string "name"
+#     t.string "email"
+#     t.string "password_digest"
+#     t.string "username"
+
+# User.create(name: "Jon Snow", email: "Kit@test.com", password: "kat", username: "Kit")
+
+# Castle.create([
+#     {
+#         name: "Glamis Castle",
+#         image: "https://images.app.goo.gl/GPwGhrsSQP6Tq7aT6",
+#         location: "Angus, Scotland, UK",
+#         short_description: "Built in 1372, a simple L-plan tower house, ancestral seat to the Earls of Strathmore and Kinghorne ",
+#         hours_of_operation: "Open Daily 10:00 ~ 17:00",
+#         website: "https://www.glamis-castle.co.uk"
+#     },
+#     {
+#         name: "Dunnottar Castle",
+#         image: "",
+#         location: "Stonehaven, Scotland, UK",
+#         short_description: "",
+#         hours_of_operation: "Open Daily 10:00 ~ 16:00",
+#         website: "https://www.dunnottarcastle.co.uk"
+#     },
+#     {
+#         name: "Stirling Castle",
+#         image: "",
+#         location: "",
+#         short_description: "",
+#         hours_of_operation: "",
+#         website: ""
+#     },
+#     {
+#         name: "Cawdor Castle",
+#         image: "",
+#         location: "",
+#         short_description: "",
+#         hours_of_operation: "",
+#         website: ""
+#     }
+# ])
