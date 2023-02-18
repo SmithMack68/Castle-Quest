061e599b6d04b5ed6aa36cfda5d436606b4f9961
Candice Smith MacKenzie <Mahony8@aol.com>
2023-02-18 02:55:40 -0500

    added material ui and changed navbvar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Navbar from './components/Navigation/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { useDispatch, useSelector } from 'react-redux';
+import Home from './components/static/Home';
+import Signup from './components/Authentication/Signup';
+import Login from './components/Authentication/Login';
+import CastleList from './features/castles/CastleList';
+import Castle from './features/castles/Castles';
+import AddCastleForm from './features/castles/AddCastleForm';
+// import EditCastle
+// import './App.css';
+
 
 function App() {
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch()
+  // })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <div className='App'> */}
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/castles" element={<CastleList />} />
+        <Route path="/castles/new" element={<AddCastleForm />} />
+        <Route path="/castles/:id" element={<Castle />} /> 
+        {/* {/* <Route path="/castles/:id/edit" element={<EditCastle />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navigation/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography style={{ fontFamily: 'cursive'}} color='black' variant="h4" className={classes.title}>
+            Castle Quest
+          </Typography>
+          <Button style={{fontFamily: ""}} color="black">Castles</Button>
+          <Button color="inherit">Trips</Button>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+
+
+// const Navbar = () => {
+//   return (
+//     <div>
+//         <h1>Navbar</h1>
+//         <ul>
+//         <nav style={{fontFamily: 'cursive', fontSize: 25}}>
+//           <li><Link to="/">Castle Quest</Link></li>
+//           </nav>
+//         </ul>
+//     </div>
+//   )
+// }
+
+// export default Navbar
